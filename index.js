c0b3c7b067ac5c46ac9bad08278ad3a3dda683e8
Voo-18 <s2221066@student.unklab.ac.id>
2025-02-12 09:22:53 +0800

    addingnewcode

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const http = require("http");
 const { hello, greetings } = require("./helloWorld");
 const moment = require("moment");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.write(hello);
-  res.write(greetings());
-  res.write(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  res.end();
-});
+app.get("/", (req, res) => res.send("Hello World"));
+app.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "About page",
+    data: [],
+  })
+);
+app.post("/contoh", (req, res) => res.send("request method POST"));
+app.put("/contoh", (req, res) => res.send("Request method PUT"));
+app.delete("/contoh", (req, res) => res.send("Request method DELETE"));
+app.patch("/contoh", (req, res) => res.send("Request method PATCH"));
+
+app.all("/universal", (req, res) => res.send(`Request method ${req.method}`));
+// Routing dinamis
+// 1. Menggunakan params
+app.get("/post/:id", (req, res) => res.send(`Artikel ke - ${req.params.id}`));
 
 const hostname = "127.0.0.1";
 const port = 3000;
-server.listen(port, hostname, () =>
+app.listen(port, hostname, () =>
   console.log(`Server running at http://${hostname}:${port}`)
 );
